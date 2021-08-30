9fd6706f0da7a57c025b51c045bfaab5d0baae17
IgorBavand <igor.bvn@gmail.com>
2021-08-30 16:01:56 -0300

    Add files via upload

---
+++ musica_buzzer.ino
@@ -0,0 +1,328 @@
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+
+//pino do buzzer
+#define pinBuz 5
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinBuz, OUTPUT);
+
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, e, 700);
+  delay(700);
+  noTone(pinBuz);
+
+  tone(pinBuz, g, 600);
+  delay(600);
+  noTone(pinBuz);
+
+  tone(pinBuz, g, 700);
+  delay(700);
+  noTone(pinBuz);
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, f, 700);
+  delay(800);
+  noTone(pinBuz);
+  
+   tone(pinBuz, f, 750);
+  delay(850);
+  noTone(pinBuz);
+
+
+  //segunda parte
+    delay(50);
+
+
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+ tone(pinBuz, g, 600);
+  delay(600);
+  noTone(pinBuz);
+
+   tone(pinBuz, g, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+
+   tone(pinBuz, f, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+   tone(pinBuz, f, 700);
+  delay(700);
+  noTone(pinBuz);
+
+
+  //tercira parte
+    delay(50);
+
+
+
+   tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, e, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, g, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, g, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, f, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+  tone(pinBuz, c, 600);
+  delay(600);
+  noTone(pinBuz);
+
+  tone(pinBuz, f, 700);
+  delay(700);
+  noTone(pinBuz);
+
+
+
+  //ultima parte 
+
+  delay(50);
+
+
+
+  tone(pinBuz, f, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, e, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, c, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+
+
+  ///repete
+
+  //tercira parte
+    delay(50);
+
+
+
+   tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, e, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, g, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, g, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, f, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+  tone(pinBuz, c, 600);
+  delay(600);
+  noTone(pinBuz);
+
+  tone(pinBuz, f, 700);
+  delay(700);
+  noTone(pinBuz);
+
+
+
+  //ultima parte 
+
+  delay(50);
+
+
+
+  tone(pinBuz, f, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, e, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+  tone(pinBuz, e, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, d, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, c, 500);
+  delay(500);
+  noTone(pinBuz);
+
+
+  tone(pinBuz, c, 600);
+  delay(600);
+  noTone(pinBuz);
+
+
+
+  
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+}
